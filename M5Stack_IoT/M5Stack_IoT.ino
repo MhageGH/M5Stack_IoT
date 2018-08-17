05e5fc2877c110113fbe4732f6a2e0266ee2f027
Mhage <masahage@soleil.ocn.ne.jp>
2018-08-17 17:43:16 +0900

    Add files via upload

---
+++ M5Stack_IoT/M5Stack_IoT.ino
@@ -0,0 +1,32 @@
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <M5Stack.h>
+#include "network_param.h"
+WiFiClientSecure httpsClient;
+PubSubClient mqttClient(httpsClient);
+
+void setup() {
+  M5.begin();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+  httpsClient.setCACert(rootCA);
+  httpsClient.setCertificate(certificate);
+  httpsClient.setPrivateKey(privateKey);
+  mqttClient.setServer(endpoint, port);
+  mqttClient.setCallback(mqttCallback);
+}
+
+void mqttCallback (char* topic, byte* payload, unsigned int length) {
+  for (int i = 0; i < length; i++) M5.Lcd.print((char)payload[i]);
+}
+
+void loop() {
+  while (!mqttClient.connected()) {
+    if (mqttClient.connect(device)) mqttClient.subscribe(subTopic, 0);
+    else delay(5000);
+  }
+  if(M5.BtnA.wasPressed()) mqttClient.publish(pubTopic, "Hello. This is M5Stack.");
+  M5.update();
+  mqttClient.loop();
+}
---
+++ M5Stack_IoT/network_param.h
@@ -0,0 +1,31 @@
+#ifndef _NETWORK_H
+#define _NETWORK_H
+
+// for WiFi
+char *ssid = "xxxx";
+char *password = "xxxx";
+
+// for AWS IoT
+const char *endpoint = "xxxx";
+const int port = 8883;
+const char *subTopic = "xxxx";
+const char *pubTopic = "xxxx";
+const char *device = "xxxx";
+
+const char* rootCA = 
+"-----BEGIN CERTIFICATE-----\n" \
+"xxxx"
+"-----END CERTIFICATE-----\n";
+
+const char* certificate = 
+"-----BEGIN CERTIFICATE-----\n" \
+"xxxx"
+"-----END CERTIFICATE-----\n";
+
+const char* privateKey = 
+"-----BEGIN RSA PRIVATE KEY-----\n" \
+"xxxx"
+"-----END RSA PRIVATE KEY-----\n";
+
+
+#endif // _NETWORK_H
